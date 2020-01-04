adce776b52dc8cbaccd6b89d011e9fc621da8fe3
Geetika Gupta <22guptageetika@gmail.com>
2020-01-04 14:54:17 -0800

    .

---
+++ webd.js
@@ -0,0 +1,100 @@
+console.log("js added");
+
+// function elementInViewport(el) {
+//   var top = el.offsetTop;
+//   var left = el.offsetLeft;
+//   var width = el.offsetWidth;
+//   var height = el.offsetHeight;
+
+//   while(el.offsetParent) {
+//     el = el.offsetParent;
+//     top += el.offsetTop;
+//     left += el.offsetLeft;
+//   }
+
+//   return (
+//     top >= window.pageYOffset &&
+//     left >= window.pageXOffset &&
+//     (top + height) <= (window.pageYOffset + window.innerHeight) &&
+//     (left + width) <= (window.pageXOffset + window.innerWidth)
+//   );
+// }
+
+// console.log(elementInViewport(document.getElementById("about")))
+
+// if(elementInViewport(document.getElementById("about"))) {
+// 	document.getElementById("about").style.visibility = "visible";
+// }
+
+
+// var isInViewport = function (elem) {
+//     var bounding = elem.getBoundingClientRect();
+//     return (
+//         bounding.top >= 0 &&
+//         bounding.left >= 0 &&
+//         bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//         bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+// };
+
+// if(isInViewport(".about")) {
+// 	document.getElementById("about").style.visibility = "visible";
+// }
+
+
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
